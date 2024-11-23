f153f90ff381694a3675aed93af5ff991344eb30
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-11-23 11:58:43 +0100

    add extra information on host and local time

--- echoserver/__init__.py
+++ echoserver/__init__.py
@@ -3,11 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    @app.route('/')
-    def index():
-        return {
-            'name': 'Wim Van den Wyngaert',
-        }
+    from echoserver.echoserver import echoserver_bp
+    app.register_blueprint(echoserver_bp)
 
     return app
