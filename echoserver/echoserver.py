48de57e9552beaedcbc135ce23260f6633f118aa
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-12-02 15:39:19 +0100

    add method, path and query_string info

--- echoserver/echoserver.py
+++ echoserver/echoserver.py
@@ -1,12 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from datetime import datetime, timezone
 import socket
 
 echoserver_bp = Blueprint('echoserver', __name__)
 
 
-@echoserver_bp.route('/')
-def index():
+@echoserver_bp.route('/', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@echoserver_bp.route('/<path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def index(path=None):
+    method = request.method
+    path = request.path
+    query_string = request.query_string.decode('utf-8')
     host_name = socket.gethostname()
     try:
         host_ip = socket.gethostbyname(host_name)
@@ -17,11 +21,16 @@ def index():
     local_time = now.astimezone().isoformat()
     local_time_utc = now.astimezone(timezone.utc).isoformat()
     result = {
-        'hostname': host_name,
-        'host_ip': host_ip,
-        'local_timezone': local_timezone,
-        'local_time': local_time,
-        'local_time_utc': local_time_utc,
+        'path': path,
+        'method': method,
+        'query_string': query_string,
+        'server_info': {
+            'hostname': host_name,
+            'host_ip': host_ip,
+            'timezone': local_timezone,
+            'time': local_time,
+            'time_utc': local_time_utc,
+        }
     }
 
     return result
